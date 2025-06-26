90ad436e28df519d40f632566efbaa13aeb4f365
cosmo-the-deer <cosmothedeer@gmail.com>
2025-06-26 16:45:52 +1200

    fixed starting bug

--- scripts/score.gd
+++ scripts/score.gd
@@ -3,7 +3,13 @@ extends Node
 @onready var label: Label = $Label
 
 var score = 0
+var first_time: bool
+
+func _ready() -> void:
+	first_time = true
 
 func _on_timer_timeout() -> void:
-	score += 1
-	label.text = str(score)
+	if not first_time:
+		score += 1
+		label.text = str(score)
+	first_time = false
